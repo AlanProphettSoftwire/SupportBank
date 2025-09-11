30dff2675e9582173667a41aa7d09685ba07a925
AlanProphettSoftwire <alan.prophett@softwire.com>
2025-09-11 15:19:21 +0100

    add AccountManager class to manage accounts and transactions

---
+++ account/accountManager.ts
@@ -0,0 +1,32 @@
+import { Account } from "./account"
+import type { TransactionRecord } from "../types/transactionRecord";
+
+export class AccountManager{
+    accounts:Map<string, Account>;
+
+    constructor(){
+        this.accounts = new Map<string, Account>();
+    }
+
+    getAccount = (account_name:string) : Account => {
+        const existingAccount = this.accounts.get(account_name);
+        if (existingAccount) return existingAccount
+
+        // account does not exist therefore make a new account 
+        const newAccount = new Account(account_name);
+        this.accounts.set(account_name, newAccount)
+        return newAccount
+    }
+
+    add_transaction_record = (transaction_record:TransactionRecord) => {
+        const from_account = this.getAccount(transaction_record.From)
+        const to_account = this.getAccount(transaction_record.To)
+
+        from_account.addTransactionOut(transaction_record)
+        to_account.addTransactionIn(transaction_record)
+    }
+
+    getAccounts = ():Map<string, Account> => {
+        return this.accounts
+    }
+}
