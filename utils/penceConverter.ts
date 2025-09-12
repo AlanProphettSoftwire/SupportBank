d0eef530fe4b26e545e053ba03a8c6e7021f15a7
AlanProphettSoftwire <alan.prophett@softwire.com>
2025-09-12 16:49:34 +0100

    rename params in convertPenceToPounds

--- utils/penceConverter.ts
+++ utils/penceConverter.ts
@@ -2,6 +2,6 @@ export function convertPoundsToPence(pounds:number){
     return pounds * 100;
 }
 
-export function convertPenceToPounds(pounds:number){
-    return pounds / 100;
+export function convertPenceToPounds(pence:number){
+    return pence / 100;
 }
